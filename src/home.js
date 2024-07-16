7c2152b227625866c13d7df53f9bbfff30c3c77b
sparky9043 <sparky9043@gmail.com>
2024-07-16 12:18:56 +0900

    Make function empty target div if not empty

--- src/home.js
+++ src/home.js
@@ -1,5 +1,20 @@
 function updateHome(contentDiv) {
-  console.log(contentDiv);
+  if (contentDiv.firstChild) contentDiv.removeChild(contentDiv.firstChild);
+
+  const homeContainer = document.createElement('div');
+  const header = document.createElement('h2');
+  const intro = document.createElement('p');
+
+  const contentArray = [header, intro];
+  
+  header.textContent = 'Hello and welcome to my restaurant!';
+  intro.textContent = 'This restaurant has been in my family for generations!';
+
+  for (const content of contentArray) {
+    homeContainer.appendChild(content);
+  }
+
+  contentDiv.appendChild(homeContainer);
 }
 
 export default updateHome;
