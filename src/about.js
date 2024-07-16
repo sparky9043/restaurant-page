03ad97e334b9f93b8dcab10c7558d8ebc24bdfc6
sparky9043 <sparky9043@gmail.com>
2024-07-16 12:24:26 +0900

    Add about button and function

--- src/about.js
+++ src/about.js
@@ -1,5 +1,20 @@
 function updateAbout(contentDiv) {
-  console.log(contentDiv);
+  if (contentDiv.firstChild) contentDiv.removeChild(contentDiv.firstChild);
+
+  const aboutContainer = document.createElement('div');
+  const header = document.createElement('h2');
+  const intro = document.createElement('p');
+
+  const contentArray = [header, intro];
+  
+  header.textContent = 'We have been in business since 1847';
+  intro.textContent = 'To talk to us, please contact us via email at fake-restaurant@fake-email.net';
+
+  for (const content of contentArray) {
+    aboutContainer.appendChild(content);
+  }
+
+  contentDiv.appendChild(aboutContainer);
 }
 
 export default updateAbout;
